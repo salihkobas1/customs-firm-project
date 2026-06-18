b8daedf24e368379732c775a3f987e9e06f3c226
salihkobas1 <salih.kobas@sabanciuniv.edu>
2026-06-18 15:15:17 +0300

    feat: vercel speed insights entegrasyonu tamamlandi

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SpeedInsights } from "@vercel/speed-insights/react"; // <-- 1. PAKETİ BURAYA İMPORT ETTİK
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights /> {/* <-- 2. BİLEŞENİ APP'İN ALTINA EKLEDİK */}
   </React.StrictMode>
 );
 
